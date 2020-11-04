6828d422c584a1b8d44f0272552a92f107685c81
derlemue <70407742+derlemue@users.noreply.github.com>
2020-11-04 23:55:54 +0100

    v1.0 | init publish

---
+++ root-server-scripts/check-nfs-mount_m.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+###############################################
+## check nfs mnt manual  v1.0 | GNU GPL 3.0  ##
+## contact:    admin@derlemue.com            ##
+##             https://discord.gg/rWpXfXHuf8 ##
+###############################################
+
+#restart every x seconds
+TIME=30
+
+#start loop starting 
+while true; do sh /root/pingmnt.sh
+echo ""
+echo "Press Ctrl-C to cancel"
+echo ""
+sleep $TIME
+done
+exit
